2691ac30afaa1428f3a501521dd742fd5e3905a7
mitya <shehovmitya@gmail.com>
2022-11-21 14:48:54 +0300

    last commit

--- statistics/DS_and_ML_intro_Stepic_4852/Stepik_ML_contest/data_preparation.py
+++ statistics/DS_and_ML_intro_Stepic_4852/Stepik_ML_contest/data_preparation.py
@@ -1,4 +1,11 @@
 import pandas as pd
+import config as conf
+
+# DATA PREPARATION
+train_events = conf.TRAIN_EVENTS
+train_submissions = conf.TRAIN_SUBMISSIONS
+test_events = conf.TEST_EVENTS
+test_submissions = conf.TEST_SUBMISSIONS
 
 
 def event_aggregation(events, submissions):
@@ -9,3 +16,7 @@ def event_aggregation(events, submissions):
 def data_preparation(events, submissions):
     user_events = event_aggregation(events, submissions)
     return user_events
+
+
+train_data = data_preparation(train_events, train_submissions)
+print(train_data.head())
