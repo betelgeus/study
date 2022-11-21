93e40e49d9c831089f7d9c3434e55778f4643792
mitya <shehovmitya@gmail.com>
2022-11-21 14:31:10 +0300

    last commit

--- statistics/DS_and_ML_intro_Stepic_4852/Stepik_ML_contest/data_preparation.py
+++ statistics/DS_and_ML_intro_Stepic_4852/Stepik_ML_contest/data_preparation.py
@@ -1,9 +1,8 @@
 import pandas as pd
-import numpy as np
 
 
 def event_aggregation(events, submissions):
-    user_events = pd.concat([events, submissions.rename(columns={'submissions_status': 'action'})])
+    user_events = pd.concat([events, submissions.rename(columns={'submission_status': 'action'})])
     return user_events
 
 
