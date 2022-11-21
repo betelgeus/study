8d5c39e684ada529a43bf5a37d5e023684ffae1f
mitya <shehovmitya@gmail.com>
2022-11-21 16:58:21 +0300

    last commit

--- statistics/DS_and_ML_intro_Stepic_4852/Stepik_ML_contest/data_preparation.py
+++ statistics/DS_and_ML_intro_Stepic_4852/Stepik_ML_contest/data_preparation.py
@@ -1,22 +1,54 @@
 import pandas as pd
 import config as conf
 
-# DATA PREPARATION
-train_events = conf.TRAIN_EVENTS
-train_submissions = conf.TRAIN_SUBMISSIONS
-test_events = conf.TEST_EVENTS
-test_submissions = conf.TEST_SUBMISSIONS
-
 
 def event_aggregation(events, submissions):
     user_events = pd.concat([events, submissions.rename(columns={'submission_status': 'action'})])
+    '''    user_events.action = pd.Categorical(user_events.action,
+                                               ['discovered', 'viewed', 'started_attempt',
+                                                'wrong', 'passed', 'correct'], ordered=True)'''
+    user_events = user_events.sort_values(['user_id', 'timestamp', 'action'])
     return user_events
+
+
+def time_preparation(events):
+    events['date'] = pd.to_datetime(events.timestamp, unit='s')
+    events['day'] = events.date.dt.date
+    return events
+
+
+def drop_duplicates(events):
+    events = events[~(events.duplicated(['user_id', 'step_id', 'action']) & events.action.eq('correct'))]
+    return events
+
+
+def course_complete(events, estimation_param, estimation_value):
+    users_course_complete = events.pivot_table(index='user_id', columns='action', values='step_id', aggfunc='count', fill_value=0).reset_index()
+    users_course_complete['course_complete'] = users_course_complete[estimation_param] > estimation_value
+    users_course_complete = users_course_complete[['user_id', 'course_complete']]
+    return users_course_complete
+
+
+def first_timestamp(events):
+    users_first_timestamp = events.groupby('user_id').aggregate({'timestamp': 'min'}).reset_index().rename(columns={'timestamp': 'first_timestamp'})
+    events = events.merge(users_first_timestamp, on='user_id', how='outer').fillna(0)
+    return events
+
+
+def metrics_n_days(events, users_course_complete, day_threshold):
+    time_threshold = day_threshold * 24 * 60 * 60
+    events['first_n_days'] = events.timestamp <= events.first_timestamp + time_threshold
+    events = events[events.first_n_days == True]
+    users_metrics_n_days = events.pivot_table(index='user_id', columns='action', values='step_id', aggfunc='count', fill_value=0).reset_index()
+    users_metrics_n_days = users_metrics_n_days.merge(users_course_complete, on='user_id', how='outer').fillna(0)
+    return users_metrics_n_days
 
 
 def data_preparation(events, submissions):
-    user_events = event_aggregation(events, submissions)
-    return user_events
-
-
-train_data = data_preparation(train_events, train_submissions)
-print(train_data.head())
+    users_events = event_aggregation(events, submissions)
+    users_events = time_preparation(users_events)
+    users_events = drop_duplicates(users_events)
+    users_course_complete = course_complete(users_events, conf.ESTIMATION_PARAM, conf.ESTIMATION_VALUE)
+    users_events = first_timestamp(users_events)
+    users_metrics_n_days = metrics_n_days(users_events, users_course_complete, conf.DAY_THRESHOLD)
+    return users_metrics_n_days
---
+++ statistics/DS_and_ML_intro_Stepic_4852/Stepik_ML_contest/features_extract.py
@@ -0,0 +1,12 @@
+from sklearn.preprocessing import StandardScaler
+
+
+scaler = StandardScaler()
+
+
+def features_extract(users_data, drop_cols=['course_complete']):
+    X = users_data.drop(drop_cols, axis=1)
+    scaler.fit(X)
+    X = scaler.transform(X)
+    y = users_data['course_complete']
+    return X, y
