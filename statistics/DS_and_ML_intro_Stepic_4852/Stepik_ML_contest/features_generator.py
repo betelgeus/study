6b1ebbeb87331e0d167f63367de4f3a4a5181b26
mitya <shehovmitya@gmail.com>
2022-11-23 19:21:36 +0300

    last commit

--- statistics/DS_and_ML_intro_Stepic_4852/Stepik_ML_contest/features_generator.py
+++ statistics/DS_and_ML_intro_Stepic_4852/Stepik_ML_contest/features_generator.py
@@ -1,6 +1,20 @@
+import numpy as np
+import pandas as pd
 import data_preparation as pr
+pd.options.mode.chained_assignment = None
 import config as conf
 from math import ceil
+
+
+def rename_columns(users_metrics_n_days):
+    users_metrics_n_days = users_metrics_n_days.rename(columns={('discovered', 1): 'd_1', ('discovered', 2): 'd_2',
+                                                                ('viewed', 1): 'v_1', ('viewed', 2): 'v_2',
+                                                                ('started_attempt', 1): 's_1',
+                                                                ('started_attempt', 2): 's_2', ('wrong', 1): 'w_1',
+                                                                ('wrong', 2): 'w_2', ('passed', 1): 'p_1',
+                                                                ('passed', 2): 'p_2', ('correct', 1): 'c_1',
+                                                                ('correct', 2): 'c_2'})
+    return users_metrics_n_days
 
 
 def users_metrics_by_days(events, users_metrics_n_days):
@@ -12,6 +26,18 @@ def users_metrics_by_days(events, users_metrics_n_days):
                                                aggfunc='count', fill_value=0).reset_index()
     users_metrics_n_days = users_metrics_n_days.merge(users_metrics_by_days, on='user_id', how='outer').fillna(0)
     users_metrics_n_days = users_metrics_n_days.set_index('user_id')
+    users_metrics_n_days = rename_columns(users_metrics_n_days)
+    return users_metrics_n_days
+
+
+def day_metrics_ratio(users_metrics_n_days):
+    users_metrics_n_days['d_ratio'] = (users_metrics_n_days.d_1 / users_metrics_n_days.d_2).fillna(0)
+    users_metrics_n_days['v_ratio'] = (users_metrics_n_days.v_1 / users_metrics_n_days.v_2).fillna(0)
+    users_metrics_n_days['s_ratio'] = (users_metrics_n_days.s_1 / users_metrics_n_days.s_2).fillna(0)
+    users_metrics_n_days['w_ratio'] = (users_metrics_n_days.w_1 / users_metrics_n_days.w_2).fillna(0)
+    users_metrics_n_days['p_ratio'] = (users_metrics_n_days.p_1 / users_metrics_n_days.p_2).fillna(0)
+    users_metrics_n_days['c_ratio'] = (users_metrics_n_days.c_1 / users_metrics_n_days.c_2).fillna(0)
+    users_metrics_n_days.replace([np.inf, -np.inf], 0, inplace=True)
     return users_metrics_n_days
 
 
@@ -23,4 +49,5 @@ def features_generator(events, submissions):
     users_course_complete = pr.course_complete(users_events, conf.ESTIMATION_PARAM, conf.ESTIMATION_VALUE)
     events, users_metrics_n_days = pr.metrics_n_days(users_events, users_course_complete, conf.DAY_THRESHOLD)
     users_metrics_n_days = users_metrics_by_days(events, users_metrics_n_days)
+    users_metrics_n_days = day_metrics_ratio(users_metrics_n_days)
     return users_metrics_n_days
