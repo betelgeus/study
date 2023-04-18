f9ebfedbfebe6902082239a3b8ae2b8c3db74c8c
mitya <shehovmitya@gmail.com>
2023-04-18 11:38:33 +0300

    Revert "Delete config.py"
    This reverts commit ecce1c41556f78d28544a67284f744f40ee68da6.

---
+++ config.py
@@ -0,0 +1,11 @@
+class Cred:
+    sql_hostname = '127.0.0.1'
+    sql_username = 'mityashekhov'
+    sql_password = '8YjTz9cyhgDJVrGoUVrr'
+    sql_main_database = 'es-orders'
+    sql_port = 3306
+    ssh_host = '138.197.179.123'
+    ssh_user = 'external'
+    ssh_port = 22
+    sql_ip = '127.0.0.1'
+    rsa_password = 'ivfj0786'
